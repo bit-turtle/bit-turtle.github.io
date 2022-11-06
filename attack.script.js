c307f160fb4db9bc373d42328d6b104352592b82
bit-turtle <109782318+bit-turtle@users.noreply.github.com>
2022-11-06 17:12:30 +0200

    Update and rename sketch.js to attack.script.js

--- attack.script.js
+++ attack.script.js
@@ -1,12 +1,27 @@
+/*
+Attack of the Cubes v1.2
+219 lines of code!
+
+Controls:
+Move mouse to move
+Click to shoot
+
+mspears.27@acsamman.edu.jo
+Give me sugestions :)
+*/
+
 var gamestarted = false;
+var gameplayed = false;
 var highscore = 0;
 var score = 0;
 var enemys = [];
+var enemysDeafeat = [];
 var lazers = [];
 var cooldown = 0;
 var difficulty = 0;
 var damage = 0;
 var lives = 3;
+var gameSpeed = 0;
 var timer = 0;
 var i;
 var i2;
@@ -18,14 +33,16 @@ function setup() {
 }
 
 function prepare() {
+  gameplayed = true;
   enemys = [];
   lazers = [];
   difficulty = 0;
   score = 0;
   damage = 0;
   lives = 3;
+  speed = 0;
   timer = 0;
-  enemys.push({x:Math.floor(Math.random()*400),y:0,type:0});
+  enemys.push({x:Math.floor(Math.random()*400),y:-10,type:0});
 }
 
 function draw() {
@@ -34,20 +51,47 @@ function draw() {
     damage-=0.6;
     cooldown-=1;
     if (lives <= 0 && damage < 0) {
+      if (score > highscore) {highscore = score;}
       gamestarted = false;
     }
     timer++;
-    if (timer >= 200) {
-      if (difficulty >= 10 && Math.floor(Math.random()*20) === 0) {  enemys.push({x:Math.floor(Math.random()*400),y:0,type:3,data:0});
+    if (timer >= 200 - gameSpeed*60) {
+      if (difficulty >= 10 && Math.floor(Math.random()*20) === 0) {  enemys.push({x:Math.floor(Math.random()*400),y:-10,type:3,sheild:1,data:0});
       }
-      else if (difficulty >= 5 && Math.floor(Math.random()*10) === 0) {  enemys.push({x:Math.floor(Math.random()*400),y:0,type:2});
+      else if (difficulty >= 5 && Math.floor(Math.random()*10) === 0) {  enemys.push({x:Math.floor(Math.random()*400),y:-10,type:2});
       }
-      else if (difficulty >= 1 && Math.floor(Math.random()*5) === 0) {  enemys.push({x:Math.floor(Math.random()*400),y:0,type:1});
+      else if (difficulty >= 1 && Math.floor(Math.random()*5) === 0) {  enemys.push({x:Math.floor(Math.random()*400),y:-10,type:1});
       }
-      else {  enemys.push({x:Math.floor(Math.random()*400),y:0,type:0});
+      else {
+        if (difficulty > 8) {
+          enemys.push({x:Math.floor(Math.random()*400),y:-10,type:1});
+        }
+        else {
+          enemys.push({x:Math.floor(Math.random()*400),y:-10,type:0});
+        }
       }
       difficulty+=0.25;
+      if (difficulty === 8) {
+        gameSpeed = 1;
+      }
+      else if (difficulty === 16) {
+        gameSpeed = 2;
+      }
       timer = 0;
+    }
+    if (mouseIsPressed && cooldown < 0) {
+      lazers.push({x:mouseX,y:400});
+      cooldown = 25;
+    }
+    for (i = 0; i < lazers.length; i++) {
+      lazers[i].y-=4;
+      fill(0,0,200);
+      rect(lazers[i].x-3,lazers[i].y-5,5,10);
+    }
+    for (i = 0; i < lazers.length; i++) {
+      if (lazers[i].y < -10) {
+        lazers.splice(i,1);
+      }
     }
     for (i = 0; i < enemys.length; i++) {
       if (enemys[i].type === 0) {
@@ -83,14 +127,19 @@ function draw() {
                enemys[i].data = 0;
             }
           }
-          if (Math.floor(Math.random()*100) === 0) {
+          if (Math.floor(Math.random()*200) === 0) {
             if (Math.floor(Math.random()*5) === 0) {  enemys.push({x:enemys[i].x,y:enemys[i].y,type:1});
             }
             else {  enemys.push({x:enemys[i].x,y:enemys[i].y,type:0});
             }
           }
         }
-        fill(255,0,0);
+        if (enemys[i].sheild === 1) {
+          fill(62,12,94);
+        }
+        else {
+          fill(255,0,0);
+        }
         rect(enemys[i].x-15,enemys[i].y-10,30,20);
       }
     }
@@ -99,15 +148,20 @@ function draw() {
         if (enemys[i].type < 3) {
           if (lazers[i2].x > enemys[i].x - 10 && lazers[i2].x < enemys[i].x + 10 && lazers[i2].y > enemys[i].y - 10 && lazers[i2].y < enemys[i].y + 10) {
             enemys.splice(i,1);
-            lazers.splice(i,1);
+            lazers.splice(i2,1);
             score++;
             break;
           }
         }
         else if (enemys[i].type === 3) {
           if (lazers[i2].x > enemys[i].x - 15 && lazers[i2].x < enemys[i].x + 15 && lazers[i2].y > enemys[i].y - 10 && lazers[i2].y < enemys[i].y + 10) {
-            enemys.splice(i,1);
-            lazers.splice(i,1);
+            if (enemys[i].sheild === 1) {
+              enemys[i].sheild = 0;
+            }
+            else {
+              enemys.splice(i,1);
+            }
+            lazers.splice(i2,1);
             score++;
             break;
           }
@@ -121,41 +175,45 @@ function draw() {
         lives -= 1;
       }
     }
-    if (mouseIsPressed && cooldown < 0) {
-      lazers.push({x:mouseX,y:400});
-      cooldown = 25;
-    }
-    for (i = 0; i < lazers.length; i++) {
-      lazers[i].y-=2;
-      fill(0,0,200);
-      rect(lazers[i].x-3,lazers[i].y-5,5,10);
-    }
-    for (i = 0; i < lazers.length; i++) {
-      if (lazers[i].y < -10) {
-        lazers.splice(i,1);
-      }
-    }
+    //healthbar
     fill(0,0,180);
     rect(mouseX-10,380,20,20);
+    fill(100,100,100);
+    rect(25,25,80,20);
+    fill(0,0,200);
+    if (lives > 0) {
+    rect(25,25,26.5*lives,20);
+    }
+    //score
+    textAlign(RIGHT);
+    textSize(30);
+    text(score,375,50)
   }
-  else {
+  //main menu
+  else if (gamestarted === false) {
+    textAlign(LEFT);
     background(0);
     if (mouseX < 280 && mouseX > 120 && mouseY < 250 && mouseY > 150) {
-      fill(220,0,0);
+      fill(200,0,0);
       if (mouseIsPressed) {
         prepare();
         gamestarted = true;
       }
     }
     else {
-      fill(240,0,0);
+      fill(220,0,0);
     }
     rect(120,150,160,100);
-    fill(255);
+    fill(0);
     textSize(40);
     text("Attack of the Cubes",25,100);
     text("Highscore: "+highscore,15,380);
-    fill(0);
     text("Start",155,210);
+    if (gameplayed) {
+      text("Score: " + score,15,320);
+      fill(200,0,0);
+      text("Game Over!",80,50);
+    }
+    
   }
 }
