86b6b9dae92b229937db42f6eccb833c7a130b5b
Michael Spears <mts18142027@gmail.com>
2024-11-01 11:55:37 -0400

    Fixed (Hopefully) PWA texteditor

--- texteditor.service.js
+++ texteditor.service.js
@@ -1,21 +1,36 @@
-const staticDevCoffee = "dev-coffee-site-v1";
-const assets = [
-	"/texteditor.html",
-	"/texteditor.png"
-];
+const CACHE_NAME = `simple-texteditor`;
 
-self.addEventListener("install", installEvent => {
-	installEvent.waitUntil(
-			caches.open(staticDevCoffee).then(cache => {
-			cache.addAll(assets)
-		})
-	)
+// Use the install event to pre-cache all initial resources.
+self.addEventListener('install', event => {
+  event.waitUntil((async () => {
+    const cache = await caches.open(CACHE_NAME);
+    cache.addAll([
+      '/',
+      '/texteditor.html',
+      '/texteditor.png'
+    ]);
+  })());
 });
 
-self.addEventListener("fetch", fetchEvent => {
-	fetchEvent.respondWith(
-		caches.match(fetchEvent.request).then(res => {
-			return res || fetch(fetchEvent.request)
-		})
-	)
+self.addEventListener('fetch', event => {
+  event.respondWith((async () => {
+    const cache = await caches.open(CACHE_NAME);
+
+    // Get the resource from the cache.
+    const cachedResponse = await cache.match(event.request);
+    if (cachedResponse) {
+      return cachedResponse;
+    } else {
+        try {
+          // If the resource was not in the cache, try the network.
+          const fetchResponse = await fetch(event.request);
+
+          // Save the resource in the cache and return it.
+          cache.put(event.request, fetchResponse.clone());
+          return fetchResponse;
+        } catch (e) {
+          // The network failed.
+        }
+    }
+  })());
 });
